ac75442ae176f577cac0fecb4cb028d6f8b9f1f6
David LePage <dwlepage70@gmail.com>
2017-01-08 12:10:28 -0600

    change url

--- tests/test_external.py
+++ tests/test_external.py
@@ -15,7 +15,7 @@ class Test(unittest.TestCase):
     print("Running Search Test..")
     def setUp(self):
         print("-------Called setup-------")
-        session.login(url='http://172.18.1.26:8082', api_key='kKphtsbQKjjfHR7amodA0001', timeout=45)
+        session.login(url='http://127.0.0.1:8082', api_key='kKphtsbQKjjfHR7amodA0001', timeout=45)
         print("Here")
     
     def tearDown(self):
