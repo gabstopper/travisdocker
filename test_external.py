eb2f1853ab7e4ee617c5d66d301d1bd58b9b8209
David LePage <dwlepage70@gmail.com>
2017-01-08 10:57:49 -0600

    Update test_external.py

--- test_external.py
+++ test_external.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     logging.getLogger()
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s')
     
-    session.login(url='http://172.18.1.26:8082', api_key='kKphtsbQKjjfHR7amodA0001', timeout=45)
+    session.login(url='http://172.18.1.26:8082', api_key='123kKphtsbQKjjfHR7amodA0001', timeout=45)
     print("Logged in!")
     session.logout()
