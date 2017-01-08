8fc9805ce434b7aa0125cd98e5af94275d3e53f0
David LePage <dwlepage70@gmail.com>
2017-01-08 12:21:29 -0600

    abc

--- tests/test_external.py
+++ tests/test_external.py
@@ -17,7 +17,7 @@ class Test(unittest.TestCase):
     time.sleep(5)
     def setUp(self):
         print("-------Called setup-------")
-        session.login(url='http://127.0.0.1:8082', api_key='kKphtsbQKjjfHR7amodA0001', timeout=45)
+        session.login(url='http://172.32.0.10:8082', api_key='kKphtsbQKjjfHR7amodA0001', timeout=45)
         print("Here")
     
     def tearDown(self):
