e5838a4ae2f65a8fac29f667a351b61323443a59
David LePage <dwlepage70@gmail.com>
2017-03-09 09:17:34 -0600

    taskrunfailed

--- tests/test_create.py
+++ tests/test_create.py
@@ -11,7 +11,7 @@ from smc.elements.helpers import zone_helper, logical_intf_helper,\
 from smc.core.engines import Layer2Firewall, Layer3Firewall, IPS, FirewallCluster,\
     MasterEngine, Engine, Layer3VirtualEngine, MasterEngineCluster
 from smc.api.exceptions import LoadEngineFailed, CreateEngineFailed,\
-    EngineCommandFailed, DeleteElementFailed, CertificateError
+    EngineCommandFailed, DeleteElementFailed, CertificateError, TaskRunFailed
 from smc.elements.network import Host, Alias, Network
 from smc.elements.other import ContactAddress
 from smc.core.interfaces import PhysicalInterface,\
